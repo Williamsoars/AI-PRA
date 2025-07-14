0a11f92a8d97b85198803b6679f36b33ee9ae805
Williamsoars <william.silva.aibox@gmail.com>
2025-07-13 22:18:24 -0300

    Create init.py

---
+++ src/init.py
@@ -0,0 +1,8 @@
+"""
+Tweet Sentiment Analysis Package
+
+This package provides tools for collecting, processing, and analyzing 
+sentiment in tweets using various NLP and machine learning techniques.
+"""
+
+__version__ = "0.1.0"
